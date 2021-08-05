b6200364b52c43fb388b401220eeca73ea0ac805
Snarls-at-Fleas <berlin.pavel@gmail.com>
2021-08-05 06:33:14 +0300

    Move script to separate file, add basic styling, coded computer choice of game move

---
+++ script.js
@@ -0,0 +1,37 @@
+function computerPlay() {
+    // Randomly choose either Rock, Paper or Scissors
+    let computerChoice = "";
+    let choiceNumber = Math.floor(Math.random() * 3) + 1;
+    switch (choiceNumber) {
+      case 1:
+        computerChoice = "Rock";
+        break;
+      case 2:
+        computerChoice = "Paper";
+        break;
+      case 3: 
+        computerChoice = "Scissors"; 
+    }
+    document.getElementById("compChoice").innerText = computerChoice;
+    // Return the chosen value
+}
+
+computerPlay();
+
+function playRound(computerChoice, humanChoice) {
+    // computerChoice is obtained from computerPlay function
+    // humanChoice is obtained from human input
+    // Check if humanChoice equals to Rock, Paper or Scissors (case insensitive)
+    // Compare humanChoice to computerChoice
+    // Display result
+}
+
+function game() {
+  // Declare humanScore & computerScore variables and initialize them with 0 value.
+  // Make 5 iteration loop
+    // Call playRound function
+    // If human won increment humanScore by 1
+    // If computer won increment computerScore by 1
+    // Display round scores
+  // Display game winner
+}
